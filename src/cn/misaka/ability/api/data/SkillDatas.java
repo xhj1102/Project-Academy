abe28ccbb02beb55fb185a5c5b194cb54146e20f
alan20091008 <1551021600@qq.com>
2014-08-04 20:34:26 +0800

    Update SkillDatas.java

--- src/cn/misaka/ability/api/data/SkillDatas.java
+++ src/cn/misaka/ability/api/data/SkillDatas.java
@@ -8,9 +8,18 @@ package cn.misaka.ability.api.data;
 */ 
  
 public class SkillDatas{
-
-
-
-
-
+	public static String player;
+	public static byte ac_level;
+	public static byte ac_class;
+	public static int ac_cp;
+	public static boolean[] ac_skill_open;
+	public static float[] ac_skill_exp;
+	public SkillDatas(String player,byte ac_level,byte ac_class,int ac_cp,boolean[] ac_skill_open,float[] ac_skill_exp){
+		this.player = player;
+		this.ac_level = ac_level;
+		this.ac_class = ac_class;
+		this.ac_cp = ac_cp;
+		this.ac_skill_open = ac_skill_open;
+		this.ac_skill_exp = ac_skill_exp;
+	}
 }
