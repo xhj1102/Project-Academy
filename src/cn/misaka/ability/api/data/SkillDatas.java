7648794a101e85414eacb010c71c2a72d73456a6
alan20091008 <1551021600@qq.com>
2014-08-04 20:37:43 +0800

    Update SkillDatas.java

--- src/cn/misaka/ability/api/data/SkillDatas.java
+++ src/cn/misaka/ability/api/data/SkillDatas.java
@@ -1,6 +1,6 @@
 package cn.misaka.ability.api.data;
 
-
+import net.minecraft.entity.player.EntityPlayer;
 /**
  * 创建这个类是为了方便管理技能
  * 以后关于传输和保存读取只需要拿他的实例当参数即可
@@ -8,12 +8,12 @@ package cn.misaka.ability.api.data;
 */ 
  
 public class SkillDatas{
-	public static String player;
-	public static byte ac_level;
-	public static byte ac_class;
-	public static int ac_cp;
-	public static boolean[] ac_skill_open;
-	public static float[] ac_skill_exp;
+	public EntityPlayer player;
+	public byte ac_level;
+	public byte ac_class;
+	public int ac_cp;
+	public boolean[] ac_skill_open;
+	public float[] ac_skill_exp;
 	public SkillDatas(String player,byte ac_level,byte ac_class,int ac_cp,boolean[] ac_skill_open,float[] ac_skill_exp){
 		this.player = player;
 		this.ac_level = ac_level;
