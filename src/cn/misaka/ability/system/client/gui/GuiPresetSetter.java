45931486e633f324470da78d0781ad35a8380a06
Xu Haoji <haoji.xu@me.com>
2014-08-23 14:00:52 +0800

    Finalize codes and implements. Everything is done!

--- src/cn/misaka/ability/system/client/gui/GuiPresetSetter.java
+++ src/cn/misaka/ability/system/client/gui/GuiPresetSetter.java
@@ -1,84 +1,96 @@
 package cn.misaka.ability.system.client.gui;
 
 import org.lwjgl.opengl.GL11;
-
+import cn.liutils.api.clinet.gui.*;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-
 
 /**
  * @author jiangyue
  *
  */
-public class GuiPresetSetter extends GuiScreen {
-	
-    private int tempX, tempY, key;
-	public final int xSizeOfTexture = 176;
-	public final int ySizeOfTexture = 88;
+
+public class GuiPresetSetter extends LIGuiScreen {
+    
+    private int skillID, subKeyID, keyID, presetID;
+    public final int xSizeOfTexture = 176;
+    public final int ySizeOfTexture = 88;
+    private LiGuiButton [] elements = new LiGuiButton [20];
     
     public GuiPresetSetter() {
-		
-        super();
+        super(xSizeOfTexture, ySizeOfTexture);
+        
+        /*  Add the selected objects to board */
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);  // TODO
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);
+        elements[0] = new LIGuiButton("", 0, 0, 0, 0);
+        /* There will be loads of code, I promise. */
+        
+        /* Then we add them to the GuiScreen. */
+        this.drawElements();
     }
-
-	@Override
-	public void initGui() {
-		//thisis.controlList.clear();
-
-		int posX = (this.width - xSizeOfTexture) / 2;
-		int posY = (this.height - ySizeOfTexture) / 2;
-
-		//this.controlList.add(new GuiButton(0, posX+ 40, posY + 40, 100, 20, "placeholder"));
-	}
+    
+    @Override
+    public void initGui() {
+        // Not used anymore.
+    }
     
     @Override
     public void drawScreen(int par1, int par2, float par3) {
-		this.drawDefaultBackground();
-        // Draws the GUI itself and handle yoooo
-		
-		//int var4 = this.mc.renderEngine.getTexture(null); // To be completed!
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		//this.mc.renderEngine.bindTexture(var4);
-
-		int posX = (this.width - xSizeOfTexture) / 2;
-		int posY = (this.height - ySizeOfTexture) / 2;
-
-		drawTexturedModalRect(posX, posY, 0, 0, xSizeOfTexture, ySizeOfTexture);
-		
-		super.drawScreen(par1, par2, par3);
+        super.drawScreen(par1, par2, par3);
     }
     
-	@Override
-	/**
-	 * 建议使用LIGuiButton
-	 */
-	protected void actionPerformed(GuiButton guibutton) {
-		if(guibutton.id >= 1 && guibutton.id <= 99) onMenuChanged(guibutton.id);
-		if(guibutton.id >= 100 && guibutton.id <= 199) onChildMenuChanged(guibutton.id - 100);
-		if(guibutton.id >= 200 && guibutton.id <= 299) {
-			onPresetKeyChanged(guibutton.id - 200);
-			confirmPresetKeyChange();
-		}
-	}
+    @Override
+    public void onButtonClicked(LiGuiButton butt, boolean is) {
+        String name = butt.name;
+        int nameInt = Integer.valueOf(name);
+        /* Use different methods to do different things. */
+        if(nameInt >= 1 && nameInt <= 99) onMenuChanged(nameInt);
+        if(nameInt >= 100 && nameInt <= 199) onChildMenuChanged(nameInt - 100);
+        if(nameInt >= 200 && nameInt <= 299) onPresetKeyChanged(nameInt - 200);
+        if(nameInt >= 300 && nameInt <= 399) {
+            onPresetChanged(nameInt - 300);
+            confirmPresetKeyChange();
+        }
+    }
     
+    
+    
+    
+    
+    
+    /**
+     * Here does the implements to the AP Controlling.
+     */
     public void onMenuChanged(int i) {
-	    this.tempX = i;
+        this.skillID = i;
     }
-
-	public void onChildMenuChanged(int i) {
-		this.tempY = i;
-	}
-
-	public void onPresetKeyChanged(int i) {
-		this.key = i;
-	}
-
-	public void confirmPresetKeyChange() {
-		invokePresetChange(this.tempX, this.tempY, this.key);
-	}
-
-	private void invokePresetChange(int x, int y, int key) {
-		// TODO Implement to the API. yoooooooooooo
-	}
-	
+    
+    public void onChildMenuChanged(int i) {
+        this.subKeyID = i;
+    }
+    
+    public void onPresetKeyChanged(int i) {
+        this.keyID = i;
+    }
+    
+    public void onPresetChanged(int i) {
+        this.presetID = i;
+    }
+    
+    /**
+     * The method connects to the AP API correctly.
+     */
+    public void confirmPresetKeyChange() {
+        invokePresetChange(this.skillID, this.subKeyID, this.keyID, this.presetID);
+    }
+    
+    public void invokePresetChange(int par1, int par2, int par3, int par4) {
+        // TODO
+    }
+    
+    
 }
